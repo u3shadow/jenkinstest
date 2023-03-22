9b41a46daa8512f7801c3162ec25054a0904c900
xiaolei <xiaolei@seeyon.com>
2023-03-22 11:16:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+import com.yinglan.scrolllayout.ScrollLayout;
+
+/**
+ * @function 次页
+ * @auther: Created by yinglan
+ * @time: 16/3/16
+ */
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+
+
+}
